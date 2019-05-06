8c9941ac0c6a163373fa21e87516016ce1bd1638
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-05-05 21:58:25 -0500

    Servidor init y creo que terminado

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+app.use(require('cors')())
+app.use(express.static('public'));
+
+
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+
+  socket.on('new-message', function(data) {
+    socket.broadcast.emit(data);
+  });
+});
+
+server.listen(8080, function() {
+  console.log("Servidor corriendo ");
+});
