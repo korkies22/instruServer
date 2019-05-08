71086f2ee7f057b18589e5e452b71d7e5740d26b
korkies22 <axficionado@hotmail.com>
2019-05-08 09:11:25 -0500

    Ahora con web sockets

--- index.js
+++ index.js
@@ -8,13 +8,27 @@ const PORT = process.env.PORT || 5000
 app.use(require('cors')())
 app.use(express.static(path.join(__dirname, 'public')))
 
+const wss = new SocketServer({ app });
+
+wss.on('connection', (ws) => {
+  console.log('Arduino connected');
+  ws.on('close', () => console.log('Arduino disconnected'));
+});
+
 io.on('connection', function(socket) {
   console.log('Alguien se ha conectado con Sockets')
 
   socket.on('message', function(data) {
       console.log('Llego '+ data)
-    socket.broadcast.emit('message',data)
+	wss.clients.forEach((client) => {
+    client.send(data);
+  });
   })
 })
+
+wss.on('message', function incoming(data) {
+  console.log(data);
+	io.sockets.emit('message',data)
+});
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`))
