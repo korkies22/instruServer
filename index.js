c192cdce31fc9c48a977cd017e13fc592a7a1ca2
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-05-08 13:04:39 -0500

    Solo websocket nativo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var app = express()
 var server = require('http').Server(app)
-var io = require('socket.io')(server, {'transports': ['websocket', 'polling']});
 const SocketServer = require('ws').Server;
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -12,24 +11,14 @@ app.use(express.static(path.join(__dirname, 'public')))
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
-  console.log('Arduino connected');
-  ws.on('close', () => console.log('Arduino disconnected'));
+  console.log('Someone connected');
+  ws.on('close', () => console.log('Someone disconnected'));
 });
 
-io.on('connection', function(socket) {
-  console.log('Alguien se ha conectado con Sockets')
-
-  socket.on('message', function(data) {
-      console.log('Llego '+ data)
-	wss.clients.forEach((client) => {
-    client.send(data);
-  });
-  })
-})
 
 wss.on('message', function incoming(data) {
-  	console.log(data);
-	io.sockets.emit('message',data)
+    console.log(data);
+    wss.send(data);
 });
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`))
